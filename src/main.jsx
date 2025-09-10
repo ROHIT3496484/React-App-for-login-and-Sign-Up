2c7810e67b35a4d63727d26a5463aca236372967
ROHIT3496484 <rohitrana349@gmail.com>
2025-09-10 23:41:37 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+const container = document.getElementById("root");
+createRoot(container).render(<App />);
